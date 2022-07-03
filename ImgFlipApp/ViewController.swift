1c5b84514872fba1306add944fdf29ee56a80990
Nikolai Maximov <kolkamaximov@gmail.com>
2022-07-03 13:35:38 +0300

    Initial Commit

---
+++ ImgFlipApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImgFlipApp
+//
+//  Created by Nikolai Maksimov on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
